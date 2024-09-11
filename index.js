91557adf9a567214b4bdb48b0abf8f7444430f61
kamilsci <sunnysideprd@gmail.com>
2024-09-10 21:12:06 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+
 // require your server and launch it here
+const server = require('./api/server');
+
+const port = 9000;
+
+// START YOUR SERVER HERE
+
+server.listen(port, ()=> {
+    console.log('listening on', port)
+})
